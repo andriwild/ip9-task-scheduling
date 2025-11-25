deed07bdc99798d328e53e721cc4c60ea9f3746f
Andri Wild <andri.wild@students.fhnw.ch>
2025-11-25 13:08:25 +0100

    add script to launch gazebo

---
+++ planner.sh
@@ -0,0 +1,5 @@
+#!/bin/zsh
+
+source /opt/ros/jazzy/setup.zsh
+source $HOME/repos/fhnw-dev-workspace/install/setup.zsh
+/home/andri/repos/fhnw-dev-workspace/src/FHNW-robot-packages/fhnw_bot_bringup/scripts/fhnw_bot_gazebo.sh
