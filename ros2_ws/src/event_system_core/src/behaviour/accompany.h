919321d819924b21ff068fcc8ac26b0e33ab1348
Andri Wild <andri.wild@students.fhnw.ch>
2026-01-15 13:37:22 +0100

    rename files

---
+++ ros2_ws/src/event_system_core/src/behaviour/accompany.h
@@ -0,0 +1,80 @@
+#pragma once
+
+#include <behaviortree_cpp/bt_factory.h>
+#include <behaviortree_cpp/blackboard.h>
+#include <behaviortree_cpp/condition_node.h>
+#include <behaviortree_cpp/basic_types.h>
+#include <memory>
+
+#include "../model/context.h"
+#include "../model/robot_state.h"
+
+
+class IsAccompany : public BT::ConditionNode {
+public:
+    IsAccompany(const std::string& name, const BT::NodeConfig& config) : BT::ConditionNode(name, config) {}
+
+    static BT::PortsList providedPorts() { return { BT::InputPort<int>("ctx") }; }
+
+    BT::NodeStatus tick() override {
+        auto ctx = config().blackboard.get()->get<std::shared_ptr<SimulationContext>>("ctx");
+        return ctx->robot->isAccompany() ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
+    }
+};
+ 
+class ArrivedWithPerson: public BT::ConditionNode {
+public:
+    ArrivedWithPerson(const std::string& name, const BT::NodeConfig& config):
+    BT::ConditionNode(name, config) {}
+
+    static BT::PortsList providedPorts() { return { BT::InputPort<int>("ctx") }; }
+
+    BT::NodeStatus tick() override {
+        auto ctx = config().blackboard.get()->get<std::shared_ptr<SimulationContext>>("ctx");
+        bool successful = true;
+        ctx->notifyLog("Arrived with person at meeting location.");
+        return successful ? BT::NodeStatus::SUCCESS : BT::NodeStatus::FAILURE;
+    }
+};
+
+class StartDropoffConversation: public BT::SyncActionNode {
+public:
+    StartDropoffConversation(const std::string& name, const BT::NodeConfig& config):
+        BT::SyncActionNode(name, config) 
+    {}
+
+    static BT::PortsList providedPorts() {
+        return { BT::InputPort<int>("ctx"), BT::InputPort<int>("current_time") };
+    }
+
+    BT::NodeStatus tick() override {
+        auto ctx        = config().blackboard.get()->get<std::shared_ptr<SimulationContext>>("ctx");
+        int currentTime = config().blackboard.get()->get<int>("current_time");
+        ctx->notifyLog("Start drop off conversation!");
+
+        ctx->queue.push(std::make_shared<DropOffConversationCompleteEvent>(currentTime + 10));
+        ctx->changeRobotState(std::make_unique<ConversateState>());
+        return BT::NodeStatus::SUCCESS;
+    }
+};
+
+class AbortAccompany: public BT::SyncActionNode {
+public:
+    AbortAccompany(const std::string& name, const BT::NodeConfig& config):
+        BT::SyncActionNode(name, config) 
+    {}
+
+    static BT::PortsList providedPorts() {
+        return { BT::InputPort<int>("ctx") };
+    }
+
+    BT::NodeStatus tick() override {
+ 
+        auto ctx = config().blackboard.get()->get<std::shared_ptr<SimulationContext>>("ctx");
+
+        ctx->notifyLog("Aborting Accompany!");
+        ctx->updateAppointmentState(des::MissionState::FAILED);
+        ctx->changeRobotState(std::make_unique<IdleState>());
+        return BT::NodeStatus::SUCCESS;
+    }
+};
