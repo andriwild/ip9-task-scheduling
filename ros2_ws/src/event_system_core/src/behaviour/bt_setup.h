0399f0c9a91fac2d07d159a70256ff675bc1c9de
Andri Wild <andri.wild@students.fhnw.ch>
2026-04-14 13:44:03 +0200

    split up behaviour tree build

--- ros2_ws/src/event_system_core/src/behaviour/bt_setup.h
+++ ros2_ws/src/event_system_core/src/behaviour/bt_setup.h
@@ -12,16 +12,74 @@
 #include "search.h"
 #include "conversation.h"
 #include "charge.h"
+#include "../plugin/order_registry.h"
 #include "mission_control.h"
 
 
 constexpr bool W_OUT_TREE   = true;
 const std::string TREE_FILE = "bt_config.xml";
 
-inline std::shared_ptr<BT::Tree> setupBehaviorTree(std::shared_ptr<ISimContext> ctx) {
-    RCLCPP_DEBUG(rclcpp::get_logger("des_application"), "Create Behaviour Tree");
-    BT::BehaviorTreeFactory factory;
+const std::string MISSION_CONTROL_ROUTINE = R"(
+  <BehaviorTree ID="MissionControlRoutine">
+    <Sequence name="Seq_HandleMissions">
+      <IsIdle/>
+      <Sequence>
+        <HasPendingMission/>
+        <Fallback name="AcceptOrDeclineMission">
+          <Sequence name="AcceptMissionSequence">
+            <MissionFeasibilityCheck/>
+            <AcceptMissionAction/>
+          </Sequence>
+          <RejectMissionAction/>
+        </Fallback>
+      </Sequence>
+    </Sequence>
+  </BehaviorTree>
+)";
 
+
+const std::string CHARGE_ROUTINE = R"(
+<BehaviorTree ID="ChargeRoutine">
+  <Fallback name="ChargeLogicTopLevel">
+
+    <Sequence name="ContinueRegularWork">
+      <Inverter><IsBatteryLow /></Inverter>
+      <Inverter><IsCharging /></Inverter>
+    </Sequence>
+
+    <IsTaskActive />
+
+    <ForceFailure>
+      <Fallback name="Execution">
+        <Sequence name="DockingSequence">
+          <GoToDock />
+          <Charge />
+        </Sequence>
+      </Fallback>
+    </ForceFailure>
+
+  </Fallback>
+</BehaviorTree>
+)";
+
+const std::string IDLE_ROUTINE = R"(
+<BehaviorTree ID="IdleRoutine">
+    <Sequence name="Seq_IdleMain">
+        <IsIdle/>
+        <Inverter>
+            <HasPendingMission/>
+        </Inverter>
+        <Inverter>
+            <IsRobotBusy/>
+        </Inverter>
+        <Docking/>
+        <EnterIdle/>
+    </Sequence>
+</BehaviorTree>
+)";
+
+
+inline void registerCoreNodes(BT::BehaviorTreeFactory& factory) {
     // charge
     factory.registerNodeType<IsBatteryLow>("IsBatteryLow");
     factory.registerNodeType<IsCharging>("IsCharging");
@@ -66,140 +124,124 @@ inline std::shared_ptr<BT::Tree> setupBehaviorTree(std::shared_ptr<ISimContext> 
     factory.registerNodeType<AcceptMissionAction>("AcceptMissionAction");
     factory.registerNodeType<RejectMissionAction>("RejectMissionAction");
     factory.registerNodeType<MissionFeasibilityCheck>("MissionFeasibilityCheck");
+}
 
-    static const char *xml_text = R"(
+
+                    // <SubTree ID="SearchRoutine" _autoremap="true"/>
+                    // <SubTree ID="ConversateRoutine" _autoremap="true"/>
+                    // <SubTree ID="AccompanyRoutine" _autoremap="true"/>
+
+inline std::string buildXml() {
+    std::string xml = R"(
      <root BTCPP_format="4" main_tree_to_execute="MainTree">
          <BehaviorTree ID="MainTree">
             <Sequence name="Seq_MainLoop">
                 <SubTree ID="ChargeRoutine" _autoremap="true"/>
                 <Fallback name="Fallback_MainStrategy">
-                    <SubTree ID="MissionControlRoutine" _autoremap="true"/>
-                    <SubTree ID="SearchRoutine" _autoremap="true"/>
-                    <SubTree ID="ConversateRoutine" _autoremap="true"/>
-                    <SubTree ID="AccompanyRoutine" _autoremap="true"/>
-                    <SubTree ID="IdleRoutine" _autoremap="true"/>
+                    <SubTree ID="MissionControlRoutine" _autoremap="true"/>)";
+
+    for (auto* plugin : OrderRegistry::instance().all()) {
+        xml += "        <SubTree ID=\"" + plugin->subtreeId() + "\" _autoremap=\"true\"/>\n";
+    }
+    
+    xml += R"(       <SubTree ID="IdleRoutine" _autoremap="true"/>
                 </Fallback>
             </Sequence>
-         </BehaviorTree>
+         </BehaviorTree>)";
 
+    xml += CHARGE_ROUTINE;
+    xml += MISSION_CONTROL_ROUTINE;
+    xml += IDLE_ROUTINE;
 
-<BehaviorTree ID="ChargeRoutine">
-  <Fallback name="ChargeLogicTopLevel">
+    for (auto* plugin : OrderRegistry::instance().all()){
+        xml += plugin->subtreeXml();
+    }
 
-    <Sequence name="ContinueRegularWork">
-      <Inverter><IsBatteryLow /></Inverter>
-      <Inverter><IsCharging /></Inverter>
-    </Sequence>
+    xml += "</root>";
+    return xml;
+}
 
-    <IsTaskActive />
+inline std::shared_ptr<BT::Tree> setupBehaviorTree(std::shared_ptr<ISimContext> ctx) {
+    RCLCPP_DEBUG(rclcpp::get_logger("des_application"), "Create Behaviour Tree");
+    BT::BehaviorTreeFactory factory;
 
-    <ForceFailure>
-      <Fallback name="Execution">
-        <Sequence name="DockingSequence">
-          <GoToDock />
-          <Charge />
-        </Sequence>
-      </Fallback>
-    </ForceFailure>
+    registerCoreNodes(factory);
 
-  </Fallback>
-</BehaviorTree>
+    for (auto* plugin : OrderRegistry::instance().all()) {
+        plugin->registeredNodes(factory);
+    }
 
-  <BehaviorTree ID="MissionControlRoutine">
-    <Sequence name="Seq_HandleMissions">
-      <IsIdle/>
-      <Sequence>
-        <HasPendingMission/>
-        <Fallback name="AcceptOrDeclineMission">
-          <Sequence name="AcceptMissionSequence">
-            <MissionFeasibilityCheck/>
-            <AcceptMissionAction/>
-          </Sequence>
-          <RejectMissionAction/>
-        </Fallback>
-      </Sequence>
-    </Sequence>
-  </BehaviorTree>
-  <BehaviorTree ID="SearchRoutine">
-    <Sequence name="Seq_SearchMain">
-      <IsSearching />
-      <Fallback name="Fallback_SearchActions">
-        <Sequence>
-          <Inverter>
-            <IsScanning />
-          </Inverter>
-          <ScanLocation />
-        </Sequence>
-        <Sequence name="Seq_FoundTarget">
-          <FoundPerson />
-          <StartAccompanyConversation />
-        </Sequence>
-        <Sequence name="Seq_NextLocation">
-          <HasNextLocation />
-          <MoveToNextLocation />
-        </Sequence>
-        <AbortSearch />
-      </Fallback>
-    </Sequence>
-  </BehaviorTree>
+    std::string xml = buildXml();
 
-        <BehaviorTree ID="ConversateRoutine">
-            <Sequence name="Seq_ConversateMain">
-                <IsConversating/>
-                <ConversationFinished/>
+  // <BehaviorTree ID="SearchRoutine">
+  //   <Sequence name="Seq_SearchMain">
+  //     <IsSearching />
+  //     <Fallback name="Fallback_SearchActions">
+  //       <Sequence>
+  //         <Inverter>
+  //           <IsScanning />
+  //         </Inverter>
+  //         <ScanLocation />
+  //       </Sequence>
+  //       <Sequence name="Seq_FoundTarget">
+  //         <FoundPerson />
+  //         <StartAccompanyConversation />
+  //       </Sequence>
+  //       <Sequence name="Seq_NextLocation">
+  //         <HasNextLocation />
+  //         <MoveToNextLocation />
+  //       </Sequence>
+  //       <AbortSearch />
+  //     </Fallback>
+  //   </Sequence>
+  // </BehaviorTree>
+
+  //       <BehaviorTree ID="ConversateRoutine">
+  //           <Sequence name="Seq_ConversateMain">
+  //               <IsConversating/>
+  //               <ConversationFinished/>
                 
-                <Fallback name="Fallback_ConverseType">
-                   <Sequence name="Seq_FoundPersonType">
-                       <IsFoundPersonConversation/>
-                       <Fallback name="Fallback_FoundPersonResult">
-                            <Sequence name="Seq_FoundPersonSuccess">
-                                <WasConversationSuccessful/>
-                                <StartAccompanyAction/>
-                            </Sequence>
-                            <!-- If conversation failed -->
-                            <AbortSearch/> 
-                       </Fallback>
-                   </Sequence>
+  //               <Fallback name="Fallback_ConverseType">
+  //                  <Sequence name="Seq_FoundPersonType">
+  //                      <IsFoundPersonConversation/>
+  //                      <Fallback name="Fallback_FoundPersonResult">
+  //                           <Sequence name="Seq_FoundPersonSuccess">
+  //                               <WasConversationSuccessful/>
+  //                               <StartAccompanyAction/>
+  //                           </Sequence>
+  //                           <!-- If conversation failed -->
+  //                           <AbortSearch/> 
+  //                      </Fallback>
+  //                  </Sequence>
 
-                   <Sequence name="Seq_DropOffType">
-                       <IsDropOffConversation/>
-                       <CompleteMissionAction/>
-                   </Sequence>
-                </Fallback>
-            </Sequence>
-        </BehaviorTree>
+  //                  <Sequence name="Seq_DropOffType">
+  //                      <IsDropOffConversation/>
+  //                      <CompleteMissionAction/>
+  //                  </Sequence>
+  //               </Fallback>
+  //           </Sequence>
+  //       </BehaviorTree>
 
-        <BehaviorTree ID="AccompanyRoutine">
-            <Sequence name="Seq_AccompanyMain">
-                <IsAccompany/>
-                <Fallback name="Fallback_AccompanyActions">
-                    <Sequence name="Seq_ArrivalAndDropOff">
-                        <ArrivedWithPerson/>
-                        <StartDropOffConversation/>
-                    </Sequence>
-                    <AbortAccompany/>
-                </Fallback>
-            </Sequence>
-        </BehaviorTree>
+  //       <BehaviorTree ID="AccompanyRoutine">
+  //           <Sequence name="Seq_AccompanyMain">
+  //               <IsAccompany/>
+  //               <Fallback name="Fallback_AccompanyActions">
+  //                   <Sequence name="Seq_ArrivalAndDropOff">
+  //                       <ArrivedWithPerson/>
+  //                       <StartDropOffConversation/>
+  //                   </Sequence>
+  //                   <AbortAccompany/>
+  //               </Fallback>
+  //           </Sequence>
+  //       </BehaviorTree>
 
-        <BehaviorTree ID="IdleRoutine">
-            <Sequence name="Seq_IdleMain">
-                <IsIdle/>
-                <Inverter>
-                    <HasPendingMission/>
-                </Inverter>
-                <Inverter>
-                    <IsRobotBusy/>
-                </Inverter>
-                <Docking/>
-                <EnterIdle/>
-            </Sequence>
-        </BehaviorTree>
-     </root>
-    )";
+  //    </root>
+    // )";
 
-    auto tree = std::make_shared<BT::Tree>(factory.createTreeFromText(xml_text));
+    factory.registerBehaviorTreeFromText(xml);
+    auto tree = std::make_shared<BT::Tree>(factory.createTree("MainTree"));
     tree->rootBlackboard()->set<std::shared_ptr<ISimContext>>("ctx", ctx);
+
     if (W_OUT_TREE) {
         std::string xml_models    = BT::writeTreeNodesModelXML(factory);
         std::string xml_full_tree = BT::WriteTreeToXML(*tree, true, true);
