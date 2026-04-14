f2e54fc205afde76615aaddef92c01747fb5d75f
Andri Wild <andri.wild@students.fhnw.ch>
2026-04-14 18:35:22 +0200

    update configs to support order types

--- ros2_ws/full.sh
+++ ros2_ws/full.sh
@@ -1,6 +1,6 @@
 #!/bin/zsh
 source /opt/ros/jazzy/setup.zsh
-source /home/andri/repos/ip9-task-scheduling/install/setup.zsh
+source /home/andri/repos/ip9-task-scheduling/ros2_ws/install/setup.zsh
 RCUTILS_COLORIZED_OUTPUT=1 \
 RCUTILS_LOGGING_MIN_SEVERITY=WARN \
 ros2 launch event_system_bringup bringup.launch.py log_level:=DEBUG mode:=full
