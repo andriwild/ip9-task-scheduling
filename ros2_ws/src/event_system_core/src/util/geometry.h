5f65dcfaf6c92e74def1641c8c0f238d9f880348
Andri Wild <andri.wild@students.fhnw.ch>
2026-07-22 16:12:43 +0200

    add geometry helpers

---
+++ ros2_ws/src/event_system_core/src/util/geometry.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include <optional>
+#include <random>
+#include <vector>
+
+#include <boost/geometry.hpp>
+#include <boost/geometry/geometries/point_xy.hpp>
+#include <boost/geometry/geometries/polygon.hpp>
+#include <boost/geometry/geometries/box.hpp>
+
+#include "types.h"
+#include "rnd.h"
+
+namespace geom {
+
+namespace bg = boost::geometry;
+using BgPoint = bg::model::d2::point_xy<double>;
+using BgPolygon = bg::model::polygon<BgPoint>;
+
+inline BgPolygon toPolygon(const std::vector<des::Point>& poly) {
+    BgPolygon p;
+    for (const auto& v : poly) {
+        bg::append(p.outer(), BgPoint(v.m_x, v.m_y));
+    }
+    bg::correct(p);
+    return p;
+}
+
+inline std::optional<des::Point> sampleInPolygon(const std::vector<des::Point>& poly, std::mt19937& rng, const int maxTries = 30) {
+    if (poly.size() < 3) {
+        return std::nullopt;
+    }
+    const BgPolygon p = toPolygon(poly);
+    bg::model::box<BgPoint> box;
+    bg::envelope(p, box);
+    const double minX = box.min_corner().x(), minY = box.min_corner().y();
+    const double maxX = box.max_corner().x(), maxY = box.max_corner().y();
+    for (int i = 0; i < maxTries; ++i) {
+        const BgPoint pt(rnd::uni(rng, minX, maxX), rnd::uni(rng, minY, maxY));
+        if (bg::within(pt, p)) {
+            return des::Point{ pt.x(), pt.y(), 0.0 };
+        }
+    }
+    return std::nullopt;
+}
+
+}  // namespace geom
