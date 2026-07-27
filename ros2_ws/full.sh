11c1761da279392f8b56f35e53936c64a4130536
Andri Wild <andri.wild@students.fhnw.ch>
2026-07-27 09:52:01 +0200

    update runners

--- ros2_ws/full.sh
+++ ros2_ws/full.sh
@@ -2,10 +2,12 @@
 source /opt/ros/jazzy/setup.zsh
 source /home/andri/repos/ip9-task-scheduling/ros2_ws/install/setup.zsh
 
+args=()
+[ -n "${1:-}" ] && args+=("config:=$1")
 
 RCUTILS_COLORIZED_OUTPUT=1 RCUTILS_LOGGING_MIN_SEVERITY=WARN \
     ros2 launch event_system_bringup bringup.launch.py mode:=full \
-        log_level:=DEBUG
+        log_level:=DEBUG "${args[@]}"
 
 # RCUTILS_COLORIZED_OUTPUT=1 RCUTILS_LOGGING_MIN_SEVERITY=WARN \
 #     ros2 launch event_system_bringup bringup.launch.py mode:=full \
--- ros2_ws/headless.sh
+++ ros2_ws/headless.sh
@@ -1,6 +1,10 @@
 #!/bin/zsh
 source /opt/ros/jazzy/setup.zsh
 source /home/andri/repos/ip9-task-scheduling/ros2_ws/install/setup.zsh
+
+args=()
+[ -n "${1:-}" ] && args+=("config:=$1")
+
 RCUTILS_COLORIZED_OUTPUT=1 \
 RCUTILS_LOGGING_MIN_SEVERITY=WARN \
-ros2 launch event_system_bringup bringup.launch.py log_level:=INFO mode:=headless rounds:=2 config:=${1:-}
+ros2 launch event_system_bringup bringup.launch.py log_level:=INFO mode:=headless rounds:=2 "${args[@]}"
